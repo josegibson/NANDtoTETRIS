aa0383dc00a2ac1a3db7d174991d28f8ff04619f
Jose Gibson <josegibson7@gmail.com>
2025-09-18 18:12:45 +0530

    refactor: encapsulate translator logic in VMTranslator class

--- Virtual Machine/main.py
+++ Virtual Machine/main.py
@@ -1,28 +1,40 @@
+import os
 import sys
 from parser import Parser
 from codewriter import CodeWriter
 
-# File validation
-filepath = sys.argv[1]
-print(filepath.split('.'))
-if filepath.split('.')[-1] != 'vm':
-    print('Provide a valid .vm file')
-    exit()
 
-parser  = Parser()
-cw = CodeWriter()
 
-vmcode = ''
-asmcode = ''
-with open(filepath, 'r') as f:
-    vmcode = f.read()
-    code_list = parser.parse(vmcode)
-    final_asm = cw.translate(code_list)
-    asmcode = '\n'.join(map(str, final_asm))
+class VMTranslator:
+    def __init__(self):
+        self.parser = Parser()
 
-outfile = filepath.split('.')[0] + '.asm'
-with open(outfile, 'w') as f:
-    f.write(asmcode)
+    def file_validation(self, filepath):
+        if filepath.split('\\')[-1].split('.')[-1] != 'vm':
+            print('Provide a valid .vm file')
+            exit()
+
+    def translate(self, filepath, outdir=None):
+        # Passing static file to name static labels
+        cw = CodeWriter(filename=filepath.split('\\')[-1].split('.')[0])
+
+        asmcode = ''
+        with open(filepath, 'r') as f:
+            vmcode = f.read()
+            code_list = self.parser.parse(vmcode)
+            final_asm = cw.translate(code_list)
+            asmcode = '\n'.join(map(str, final_asm))
+
+
+        outfile = filepath.split('\\')[-1].split('.')[0] + '.asm'
+        if outdir != None:
+            outfile = os.path.join(outdir, outfile)
+        
+        with open(outfile, 'w') as f:
+            f.write(asmcode)
     
 
 
+if __name__ == "__main__":
+    vmt = VMTranslator()
+    vmt.translate(sys.argv[1])
