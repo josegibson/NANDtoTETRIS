842fa31019a3ea100640b194aa5d62db7916d64a
Jose Gibson <josegibson7@gmail.com>
2025-09-19 12:46:40 +0530

    feat(test): add initial test suite for vmtranslator

---
+++ VirtualMachine/test/test_vmt.py
@@ -0,0 +1,38 @@
+import os, sys, pytest
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from main import VMTranslator
+
+input_path = os.path.join(os.path.dirname(__file__), "project_docs")
+output_path = os.path.join(os.path.dirname(__file__), "output")
+expected_output_path = os.path.join(os.path.dirname(__file__), "expected_output")
+
+
+@pytest.mark.parametrize("test_name", [
+    "BasicTest",
+    "PointerTest",
+    "SimpleAdd",
+    "StackTest",
+    "StaticTest"
+])
+def test_translation(test_name):
+
+    input = os.path.join(input_path, f"{test_name}", f"{test_name}.vm")
+    output = os.path.join(output_path, f'{test_name}.asm')
+    expected_output = os.path.join(expected_output_path, f"{test_name}.asm")
+
+    vmt = VMTranslator()
+        
+    vmt.translate(input, outdir=output_path)
+
+    out = ""
+    with open(output) as f:
+        out = f.read()
+
+    expected_out = ""
+    with open(expected_output) as f:
+        expected_out = f.read()
+
+    assert out == expected_out
+
