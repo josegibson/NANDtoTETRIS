e81b9d43ad26f2e29f6a4393e5b4637d9deb7150
Jose Gibson <josegibson7@gmail.com>
2025-09-19 17:09:07 +0530

    refactor(compiler): simplify symbol table architecture

--- Compiler/SymbolTable.py
+++ Compiler/SymbolTable.py
@@ -5,7 +5,6 @@ class SymbolTable:
         self.class_name = name
         self.class_scope = {}
         self.subroutine_signatures = {}
-        self.class_index_counter = 0
 
         self.subroutine_name = None
         self.subroutine_type = None
@@ -14,7 +13,7 @@ class SymbolTable:
         
         self.index_counters = {'static': 0, 'field': 0,'var': 0, 'arg': 0}
           
-    def startSubroutine(self, name):
+    def startSubroutine(self, name, kind, type):
 
         # print(f'Subroutine name: {self.subroutine_name}')
         # print(f'Subroutine scope')
@@ -23,6 +22,8 @@ class SymbolTable:
         # print()
 
         self.subroutine_name = name
+        self.subroutine_kind = kind
+        self.subroutine_type = type
         self.subroutine_scope = {}
         self.subroutine_label_counter = 0
         self.index_counters['var'] = 0
@@ -32,6 +33,7 @@ class SymbolTable:
         return symbol in self.subroutine_scope or symbol in self.class_scope
 
     def define_signature(self, name, kind, type, nArgs, nLocals):
+        '''Only used for debugging purposes'''
         entry = {'kind': kind, 'type': type, 'nArgs': nArgs, 'nLocals': nLocals}
 
         if name in self.subroutine_signatures:
@@ -47,27 +49,19 @@ class SymbolTable:
             raise ValueError(f'Subroutine: "{name}" not found in subroutine signatures')
         
     def define(self, name, type, kind):
-        entry = {'kind': kind, 'type': type, 'index': None}
+        entry = {'kind': kind, 'type': type, 'index': self.index_counters[kind]}
 
-        if entry['kind'] in ['static','field']:
-            entry['index'] = self.class_index_counter
-            self.index_counters[entry['kind']] += 1
-            self.class_index_counter += 1
+        self.index_counters[entry['kind']] += 1
 
-            if name in self.class_scope:
-                raise ValueError(f'Symbol defined more that once in the class scope: {name}')
-
-            self.class_scope[name] = entry
-        elif entry['kind'] in ['var', 'arg']:
-            entry['index'] = self.index_counters[entry['kind']]
-            self.index_counters[entry['kind']] += 1
-
-            if name in self.subroutine_scope:
-                raise ValueError(f'Symbol defined more that once in the subroutine scope: {name}')
-    
-            self.subroutine_scope[name] = entry
-        else:
+        scope = None
+        if entry['kind'] in ['static', 'field']  and name in self.class_scope:
+            raise ValueError(f'Symbol defined more that once in the class scope: {name}')
+        elif entry['kind'] in ['var', 'arg'] and name in self.subroutine_scope:
+            raise ValueError(f'Symbol defined more that once in the subroutine scope: {name}')
+        elif entry['kind'] not in ['static', 'field', 'var', 'arg']:
             raise KeyError(f"Invalid kind; variable '{name}' has an invalid kind: '{entry['kind']}'")
+
+        self.class_scope[name] = entry            
         
     def varCount(self, kind):
         return self.index_counters[kind]
