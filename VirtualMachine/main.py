52b7bcaf634d74a672366138a0150d5d9e2157ff
Jose Gibson <josegibson7@gmail.com>
2025-09-19 16:04:28 +0530

    refactor(translator): centralize CodeWriter for multi-file state

--- VirtualMachine/main.py
+++ VirtualMachine/main.py
@@ -28,48 +28,25 @@ class VMTranslator:
         
         
         final_asmcode = []
-
+        codew = CodeWriter()
         for vmfile in target_files:
-            codew = CodeWriter(os.path.splitext(os.path.basename(vmfile))[0])
+            filename = os.path.splitext(os.path.basename(vmfile))[0]
             with open(vmfile, 'r') as f:
                 vmcode = self.parser.parse(f.read())
-                asmcode = codew.translate(vmcode)
+                asmcode = codew.translate(vmcode, filename)
                 if os.path.basename(vmfile) == 'Sys.vm':
                     final_asmcode = asmcode + final_asmcode
                 else:
-                    final_asmcode.append(asmcode)
+                    final_asmcode.extend(asmcode)
 
         
-        final_asmcode = '/n'.join(final_asmcode)
+        final_asmcode = '\n'.join(final_asmcode)
 
         asmfile = os.path.splitext(os.path.basename(target))[0] + '.asm'
         if self.dest_dir:
             asmfile = os.path.join(self.dest_dir, asmfile)
         with open(asmfile, 'w') as f:
             f.write(final_asmcode)
-
-
-
-    def translate(self, filepath, outdir=None):
-        # Passing static file to name static labels
-        filename = os.path.basename(filepath)
-        cw = CodeWriter(filename=os.path.splitext(filename)[0])
-
-        asmcode = ''
-        with open(filepath, 'r') as f:
-            vmcode = f.read()
-            code_list = self.parser.parse(vmcode)
-            final_asm = cw.translate(code_list)
-            asmcode = '\n'.join(map(str, final_asm))
-
-
-        outfile = os.path.splitext(filename)[0] + '.asm'
-        if outdir != None:
-            outfile = os.path.join(outdir, outfile)
-        
-        with open(outfile, 'w') as f:
-            f.write(asmcode)
-    
 
 
 if __name__ == "__main__":
